8d4f88763e577eba61176154e0024aae4f692bd0
ranjithSE <ranjithtamizhchelvan@gmail.com>
2019-03-07 09:08:36 +0000

    cucumber add director scenario

--- rottenpotatoes/features/support/paths.rb
+++ rottenpotatoes/features/support/paths.rb
@@ -20,6 +20,9 @@ module NavigationHelpers
     #
     #   when /^(.*)'s profile page$/i
     #     user_profile_path(User.find_by_login($1))
+    when /^the edit page for "(.*)"$/
+      movie = Movie.find_by(title: $1)
+      edit_movie_path(movie)     
 
     else
       begin
